f93cceed7f240e5126f077e9312c513676519576
dougreisss <douglas_rrs@hotmail.com>
2023-08-09 09:15:29 -0300

    Initial commt

---
+++ Aula1/Projeto.AspNet.01.MVC/Models/Perfil.cs
@@ -0,0 +1,29 @@
+﻿namespace Projeto.AspNet._01.MVC.Models
+{
+    // esta classe assume o "papel" de model domain da aplicação.
+    // Isso significa que: as regras/formatos com quais os dados serão manipulados pela aplicação
+    // ficam estabelecidas aqui.
+    public class Perfil
+    {
+        // definir as props/atributos de classe
+
+        /* 
+         
+            private string _nome;
+
+            public string? Nome { 
+            
+                // uso dos métodos acessores
+
+                get { return _nome;  } 
+                set { _nome = value; }
+
+            }
+        
+        */
+
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+        public string? Endereco { get; set; }
+    }
+}
