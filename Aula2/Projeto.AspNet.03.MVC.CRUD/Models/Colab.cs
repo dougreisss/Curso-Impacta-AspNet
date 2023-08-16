16f9ec4d9629e0fc63b64644559a94b0f3a47ae4
dougreisss <douglas_rrs@hotmail.com>
2023-08-16 11:52:02 -0300

    add data annotation  and css layout

--- Aula2/Projeto.AspNet.03.MVC.CRUD/Models/Colab.cs
+++ Aula2/Projeto.AspNet.03.MVC.CRUD/Models/Colab.cs
@@ -1,11 +1,17 @@
-﻿namespace Projeto.AspNet._03.MVC.CRUD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.AspNet._03.MVC.CRUD.Models
 {
     public class Colab
     {
+        [Required(ErrorMessage = "Insira, por favor, seu nome.")]
         public string? Nome { get; set; }
+        [Range(15, 99, ErrorMessage = "Informe, por favor, uma idade entre 15 e 99 anos.")]
         public int Idade { get; set; }
+        [RegularExpression(@"\d+(\.\d{1, 2})?", ErrorMessage = "Valor invalido. Sugestão: insira dessa forma $ ou $.$")]
         public decimal Salario { get; set; }
         public string? Departamento { get; set; }
+        [RegularExpression(@"^[FMO]+$", ErrorMessage = "Selecione ao menos um valor.")]
         public Char Genero { get; set; }
     }
 }
